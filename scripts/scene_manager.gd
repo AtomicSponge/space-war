550ea8c914c91d7680f914a98530de17643bfdae
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-15 14:54:42 -0400

    sync

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,10 +1,14 @@
 extends Node
 
+# A collection of scenes in the game. Scenes are added through the Inspector panel
+@export var Scenes = {}
+
+# Alias of the currently selected scene
+var m_CurrentSceneAlias = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:

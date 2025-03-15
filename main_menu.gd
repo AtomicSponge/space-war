bdc83ab020df38a22b928b331fecfed6a05f79f5
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-15 14:00:50 -0400

    sync

--- main_menu.gd
+++ main_menu.gd
@@ -10,8 +10,6 @@ var current_option = 0
 # Called when the node enters the scene tree for the first time.
 # Hide the left/right option btns until a mouse enters them.
 func _ready() -> void:
-	option_left_btn.modulate.a = 0
-	option_right_btn.modulate.a = 0
 	main_btn.text = MENU_OPTIONS[current_option]
 	pass
 
@@ -35,22 +33,6 @@ func _on_main_button_pressed() -> void:
 			# Quit
 			get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 			get_tree().quit()
-
-# Mouse entered left button - show
-func _on_option_left_button_mouse_entered() -> void:
-	option_left_btn.modulate.a = 255
-
-# Mouse entered right button - show
-func _on_option_right_button_mouse_entered() -> void:
-	option_right_btn.modulate.a = 255
-
-# Mouse exited left button - hide
-func _on_option_left_button_mouse_exited() -> void:
-	option_left_btn.modulate.a = 0
-
-# Mouse exited right button - hide
-func _on_option_right_button_mouse_exited() -> void:
-	option_right_btn.modulate.a = 0
 
 # Left option button pressed
 func _on_option_left_button_pressed() -> void:

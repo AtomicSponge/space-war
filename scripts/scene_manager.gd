3bc984a5b21c3cbd0cc521021c714d933adda467
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-16 09:19:22 -0400

    sync

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -6,10 +6,40 @@ extends Node
 # Alias of the currently selected scene
 var m_CurrentSceneAlias = ""
 
-# Called when the node enters the scene tree for the first time.
+# Description: Find the initial scene as defined in the project settings
 func _ready() -> void:
-	pass # Replace with function body.
+	var mainScene = ProjectSettings.get_setting("application/run/main_scene")
+	m_CurrentSceneAlias = Scenes.find_key(mainScene)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Description: Add a new scene to the scene collection
+# Parameter sceneAlias: The alias used for finding the scene in the collection
+# Parameter scenePath: The full path to the scene file in the file system
+func AddScene(sceneAlias: String, scenePath: String) -> void:
+	Scenes[sceneAlias] = scenePath
+	
+# Description: Remove an existing scene from the scene collection
+# Parameter sceneAlias: The scene alias of the scene to remove from the collection
+func RemoveScene(sceneAlias: String) -> void:
+	Scenes.erase(sceneAlias)
+	
+# Description: Switch to the requested scene based on its alias
+# Parameter sceneAlias: The scene alias of the scene to switch to
+func SwitchScene(sceneAlias: String) -> void:
+	get_tree().change_scene_to_file(Scenes[sceneAlias])
+
+# Description: Restart the current scene
+func RestartScene() -> void:
+	get_tree().reload_current_scene()
+	
+# Description: Quit the game
+func QuitGame() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
+	
+# Description: Return the number of scenes in the collection
+func GetSceneCount() -> int:
+	return Scenes.size()
+	
+# Description: Returns the alias of the current scene
+func GetCurrentSceneAlias() -> String:
+	return m_CurrentSceneAlias

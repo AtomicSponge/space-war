8cf9e0b2602107016d3bea42c1ddcdf878bd7e18
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-16 12:39:59 -0400

    sync

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -6,3 +6,15 @@ extends Node
 var PlayerName: String = ""
 var PlayerLives: int = 0
 var PlayerExperience: float = 0
+
+# Load game settings - called during startup
+func LoadGameSettings() -> void:
+	pass
+
+# Save game settings
+func SaveGameSettings() -> void:
+	pass
+
+#  Load game settings on start-up
+func _ready() -> void:
+	LoadGameSettings()

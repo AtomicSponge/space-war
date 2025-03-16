1064d40b13c5938c01fe791e6da581fc3f03c40c
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-16 11:01:01 -0400

    sync

--- scripts/high_scores.gd
+++ scripts/high_scores.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,3 +7,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _on_back_button_pressed() -> void:
+	SceneManager.SwitchScene("MainMenu")

166bde11e35d2486ffacbb85c3025a4219d06b0a
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-17 10:11:20 -0400

    sync

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,12 +2,18 @@ extends Node
 
 @onready var MainBtn = $MarginContainer/VBoxContainer/CenterContainerBottom/HBoxContainer/MainButton
 
-const MenuOptions = [ "New Game", "High Scores", "Options", "Quit" ]
+var MenuOptions = [ 
+	load("res://gfx/new_game_btn.png"),
+	load("res://gfx/high_scores_btn.png"),
+	load("res://gfx/options_btn.png"),
+	load("res://gfx/quit_btn.png")
+]
 var CurrentOption: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	MainBtn.text = MenuOptions[CurrentOption]
+	MainBtn.texture_normal = MenuOptions[CurrentOption]
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -33,10 +39,10 @@ func _on_main_button_pressed() -> void:
 func _on_option_left_button_pressed() -> void:
 	if CurrentOption > 0:
 		CurrentOption = CurrentOption - 1
-		MainBtn.text = MenuOptions[CurrentOption]
+		MainBtn.texture_normal = MenuOptions[CurrentOption]
 
 # Right option button pressed
 func _on_option_right_button_pressed() -> void:
 	if CurrentOption < MenuOptions.size() - 1:
 		CurrentOption = CurrentOption + 1
-		MainBtn.text = MenuOptions[CurrentOption]
+		MainBtn.texture_normal = MenuOptions[CurrentOption]

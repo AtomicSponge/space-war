d10fb8a0b247988f832d96ae5cbd9d3f2f7ec051
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-17 12:02:33 -0400

    sync

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -8,14 +8,18 @@ var PlayerLives: int = 0
 var PlayerExperience: float = 0
 
 # Private variables
-const _save_path = "user://save.dat"
+const _save_path = "user://game.dat"
 
 # Load game settings - called during startup
-func LoadGameSettings() -> void:
-	var file = FileAccess.open_encrypted_with_pass(_save_path, FileAccess.READ, "changeme")
+func LoadGameData() -> void:
+	if not FileAccess.file_exists(_save_path):
+		return
+	var file = FileAccess.open_encrypted_with_pass(_save_path, FileAccess.READ, "dragongelatohierarchy")
+	var save_data = file.get_var()
+	file.close()
 
 # Save game settings
-func SaveGameSettings() -> void:
+func SaveGameData() -> void:
 	var save_data = {
 		"settings": {},
 		"high_scores": [
@@ -23,10 +27,12 @@ func SaveGameSettings() -> void:
 		]
 	}
 	
-	var file = FileAccess.open_encrypted_with_pass(_save_path, FileAccess.WRITE, "changeme")
+	var file = FileAccess.open_encrypted_with_pass(_save_path, FileAccess.WRITE, "dragongelatohierarchy")
+	if not FileAccess.file_exists(_save_path):
+		return
 	file.store_var(save_data)
 	file.close()
 
 #  Load game settings on start-up
 func _ready() -> void:
-	pass
+	LoadGameData()

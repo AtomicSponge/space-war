0d422fb3b67e28acc10df4ed23b083592e4bf417
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-17 11:41:33 -0400

    sync

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -7,15 +7,26 @@ var PlayerName: String = ""
 var PlayerLives: int = 0
 var PlayerExperience: float = 0
 
+# Private variables
+const _save_path = "user://save.dat"
+
 # Load game settings - called during startup
 func LoadGameSettings() -> void:
-	pass
+	var file = FileAccess.open_encrypted_with_pass(_save_path, FileAccess.READ, "changeme")
 
 # Save game settings
 func SaveGameSettings() -> void:
-	#const data = {}
-	pass
+	var save_data = {
+		"settings": {},
+		"high_scores": [
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		]
+	}
+	
+	var file = FileAccess.open_encrypted_with_pass(_save_path, FileAccess.WRITE, "changeme")
+	file.store_var(save_data)
+	file.close()
 
 #  Load game settings on start-up
 func _ready() -> void:
-	LoadGameSettings()
+	pass

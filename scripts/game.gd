09b37c4cbfc45969ff7645909cdd9e9d7d935e7a
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-19 16:15:49 -0400

    sync

--- scripts/game.gd
+++ scripts/game.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	# Pause game
 	if Input.is_action_pressed("pause_game"):
 		get_tree().paused = true
 		$PauseMenu.show()

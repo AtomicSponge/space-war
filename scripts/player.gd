13201e2b6e08618a547f37221707f0ce44583db5
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-19 15:04:24 -0400

    sync

---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		$Sprite2D.flip_v = false
+		$Sprite2D.flip_h = true
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		$Sprite2D.flip_v = true
+		$Sprite2D.flip_h = true
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		$Sprite2D.flip_v = true
+		$Sprite2D.flip_h = false
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		$Sprite2D.flip_v = false
+		$Sprite2D.flip_h = false

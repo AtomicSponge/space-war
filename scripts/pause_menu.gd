51301970531ef554ba6deecf4215c80fb1bde03a
Matthew Evans <mevans+git@wtfsystems.net>
2025-03-23 09:34:22 -0400

    sync

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,12 +1,4 @@
-extends Control
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+extends ColorRect
 
 # Resume button pressed
 func _on_resume_button_pressed() -> void:

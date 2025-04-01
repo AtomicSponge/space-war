cc08d012f7426278c1a442b7d8239fd6bf045749
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-01 11:46:16 -0400

    sync

--- scripts/game.gd
+++ scripts/game.gd
@@ -68,7 +68,7 @@ func _process(_delta: float) -> void:
 			Continue.ContinueTimer.start()
 			await Continue.ContinueTimer.timeout
 			get_tree().paused = false
-			# Continue selected, resume game
+			# Continue selected, resume gamed
 			if Continue.ContinueSelected:
 				Player.show()
 				GameState.PlayerLives = GameState.NumberLives

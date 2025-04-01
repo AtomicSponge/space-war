d2868b82be21338a48bca28ee4156240c1a8e2f8
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-01 11:40:23 -0400

    sync

--- scripts/game.gd
+++ scripts/game.gd
@@ -55,7 +55,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# Game not running, start it.
-	if GameStarted == false:
+	if not GameStarted:
 		NewGame()
 
 	# Player lost, end game

f286df0c7f3b4f7e03e0d21d4e24ffa46bdf2101
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-02 13:09:46 -0400

    sync

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -2,7 +2,10 @@ extends Node
 
 # Array[{"time" (float), "type" (int), "location" (Vector2)}]
 const SpawnQueue:Array[Dictionary] = [
-	{ "time": 300.0, "type": 0, "location": Vector2(0, 0) }
+	{ "time": 180.0, "type": 0, "location": Vector2(0, 0) },
+	{ "time": 180.0, "type": 0, "location": Vector2(0, 0) },
+	{ "time": 180.0, "type": 0, "location": Vector2(0, 0) },
+	{ "time": 180.0, "type": 0, "location": Vector2(0, 0) }
 ]
 
 # Get all enemies for the current time

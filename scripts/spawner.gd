ff82f4221b453af945b20cfe86ae2e950413e0a6
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-02 10:33:34 -0400

    sync

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,5 +1,14 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var SpawnQueue:Array[Dictionary] = []
+
+const _queue_path: String = "res://spawner.dat"
+
+func LoadScript() -> int:
+	var file = FileAccess.open_encrypted_with_pass(_queue_path, FileAccess.READ, "dragongelatohierarchy")
+	if file == null:
+		GameState.alert('Could not load spawner data!')
+		return -1
+	SpawnQueue = file.get_var()
+	file.close()
+	return 0

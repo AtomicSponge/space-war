5d913b71e451182ae070ed12e69c009773e49059
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-02 18:58:05 -0400

    sync

--- scripts/enemy_0.gd
+++ scripts/enemy_0.gd
@@ -2,10 +2,20 @@ extends Area2D
 
 @export var Bullet: PackedScene
 
+@onready var EnemyHitbox: CollisionShape2D = $EnemyHitbox
+@onready var TowerSprite: Sprite2D = $TowerSprite
+@onready var CannonSprite: Sprite2D = $CannonSprite
+@onready var TowerAnimationPlayer: AnimationPlayer = $TowerSprite/TowerAnimationPlayer
+@onready var CannonAnimationPlayer: AnimationPlayer = $CannonSprite/CannonAnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	EnemyHitbox.disabled = true
+	TowerAnimationPlayer.play("Fade")
+	CannonAnimationPlayer.play("Fade")
+	await TowerAnimationPlayer.animation_finished
+	EnemyHitbox.disabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass

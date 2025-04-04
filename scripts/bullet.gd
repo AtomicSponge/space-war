13058be63aa72b20c2b8cd13c96ea48d932ae9db
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-03 20:09:03 -0400

    sync

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -14,8 +14,6 @@ func _process(delta: float) -> void:
 		queue_free()
 	position += transform.x * speed * delta
 
-#
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemies"):
-		body.queue_free()
-	queue_free()
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemies"):
+		pass
--- scripts/enemy_bullet.gd
+++ scripts/enemy_bullet.gd
@@ -15,6 +15,6 @@ func _process(delta: float) -> void:
 	position += transform.x * speed * delta
 
 #
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		queue_free()
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		pass

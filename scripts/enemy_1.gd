6a0de95f9a3ca3a7a0b9b9aaab91133b526df3d6
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-07 09:39:53 -0400

    sync

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-var _is_ready: bool = false
+@export var speed: int = 200
 
 @onready var ShipAnimationPlayer: AnimationPlayer = $ShipSprite/ShipAnimationPlayer
 @onready var EnemyHitbox: CollisionShape2D = $EnemyHitbox
+
+var _is_ready: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,8 +18,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	look_at(GameState.PlayerLocation)
 	if not _is_ready:
 		return
 
-func _take_damage() -> void:
-	pass
+#
+func _take_damage(testName: StringName) -> void:
+	if name == testName:
+		queue_free()
+
+#
+func _on_area_entered(area: Area2D) -> void:
+	# Collided with player
+	if area.name == "Player":
+		Events.player_hit.emit()
+		queue_free()

0f0bf0b68a44793f7de6734afd38ac9a721b29fa
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-07 09:31:37 -0400

    sync

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -2,10 +2,22 @@ extends Area2D
 
 var _is_ready: bool = false
 
+@onready var ShipAnimationPlayer: AnimationPlayer = $ShipSprite/ShipAnimationPlayer
+@onready var EnemyHitbox: CollisionShape2D = $EnemyHitbox
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Events.enemy_hit.connect(_take_damage)
+	EnemyHitbox.set_deferred("disabled", true)
+	ShipAnimationPlayer.play("Fade")
+	await ShipAnimationPlayer.animation_finished
+	EnemyHitbox.set_deferred("disabled", false)
+	_is_ready = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if not _is_ready:
+		return
+
+func _take_damage() -> void:
 	pass

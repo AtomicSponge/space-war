5547eeded1f4ba5e546bf433fbd83f8172a65191
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-09 11:48:35 -0400

    sync

--- scripts/enemy_2.gd
+++ scripts/enemy_2.gd
@@ -1,13 +1,21 @@
 extends RigidBody2D
 
 @export var Explosion: PackedScene
+@export var Health: int = 50
 @export var ScoreValue: int = 250
+
+@onready var ShipSprite: Sprite2D = $ShipSprite
+@onready var ShipAnimationPlayer: AnimationPlayer = $ShipSprite/ShipAnimationPlayer
+@onready var EnemyHitbox: CollisionShape2D = $EnemyHitbox
 
 var _is_ready: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Fade in animation
+	EnemyHitbox.set_deferred("disabled", true)
+	#ShipAnimationPlayer.play("Fade")
+	#await ShipAnimationPlayer.animation_finished
+	EnemyHitbox.set_deferred("disabled", false)
 	Events.enemy_hit.connect(_take_damage)
 	_is_ready = true
 
@@ -16,16 +24,19 @@ func _process(delta: float) -> void:
 	pass
 
 # Hit
-func _take_damage(testName: StringName, _amount: int, bulletFlag: bool) -> void:
+func _take_damage(testName: StringName, amount: int, bulletFlag: bool) -> void:
 	if name == testName:
+		Health -= amount
+		#ShipAnimationPlayer.play("Flash")
+	if Health == 0:
 		_is_ready = false
-		#EnemyHitbox.set_deferred("disabled", true)
+		EnemyHitbox.set_deferred("disabled", true)
 		if bulletFlag == true:
 			GameState.PlayerScore += ScoreValue
-		#ShipSprite.hide()
-		#var explosionEffect = Explosion.instantiate()
-		#add_child(explosionEffect)
-		#explosionEffect.global_position = position
-		#explosionEffect.emitting = true
+		ShipSprite.hide()
+		var explosionEffect = Explosion.instantiate()
+		add_child(explosionEffect)
+		explosionEffect.global_position = position
+		explosionEffect.emitting = true
 		await get_tree().create_timer(1.0).timeout
 		queue_free()

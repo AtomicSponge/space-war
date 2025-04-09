02eb340a2463cfb3c97d6244ccd3f1390fadc687
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-09 11:40:43 -0400

    sync

--- scripts/enemy_2.gd
+++ scripts/enemy_2.gd
@@ -1,9 +1,31 @@
 extends RigidBody2D
 
+@export var Explosion: PackedScene
+@export var ScoreValue: int = 250
+
+var _is_ready: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Fade in animation
+	Events.enemy_hit.connect(_take_damage)
+	_is_ready = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+# Hit
+func _take_damage(testName: StringName, _amount: int, bulletFlag: bool) -> void:
+	if name == testName:
+		_is_ready = false
+		#EnemyHitbox.set_deferred("disabled", true)
+		if bulletFlag == true:
+			GameState.PlayerScore += ScoreValue
+		#ShipSprite.hide()
+		#var explosionEffect = Explosion.instantiate()
+		#add_child(explosionEffect)
+		#explosionEffect.global_position = position
+		#explosionEffect.emitting = true
+		await get_tree().create_timer(1.0).timeout
+		queue_free()

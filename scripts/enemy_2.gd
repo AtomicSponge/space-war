fc92f73b371c191338a12680f8aba3f4a4b2f5a8
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-14 10:17:11 -0400

    sync

--- scripts/enemy_2.gd
+++ scripts/enemy_2.gd
@@ -1,13 +1,14 @@
-extends Area2D
+extends Path2D
 
 @export var Explosion: PackedScene
 @export var Health: int = 50
 @export var ScoreValue: int = 250
 @export var speed: float = 0.2
 
-@onready var ShipSprite: Sprite2D = $ShipSprite
-@onready var ShipAnimationPlayer: AnimationPlayer = $ShipSprite/ShipAnimationPlayer
-@onready var EnemyHitbox: CollisionShape2D = $EnemyHitbox
+@onready var EnemyPathA: PathFollow2D = $EnemyPathA
+@onready var ShipSprite: Sprite2D = $EnemyPathA/ShipA/ShipSprite
+@onready var ShipAnimationPlayer: AnimationPlayer = $EnemyPathA/ShipA/ShipSprite/ShipAnimationPlayer
+@onready var EnemyHitbox: CollisionShape2D = $EnemyPathA/ShipA/EnemyHitbox
 
 var _target_progress: float = 0.99
 var _is_ready: bool = false
@@ -21,25 +22,24 @@ func _ready() -> void:
 	await ShipAnimationPlayer.animation_finished
 	EnemyHitbox.set_deferred("disabled", false)
 	Events.enemy_hit.connect(_take_damage)
-	# works but maybe a better solution?
-	name = get_parent().get_parent().name
 	_is_ready = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not _is_ready:
 		return
-	if get_parent().progress_ratio < _target_progress:
-		get_parent().progress_ratio += delta * speed
+	if EnemyPathA.progress_ratio < _target_progress:
+		EnemyPathA.progress_ratio += delta * speed
 		ShipSprite.flip_h = false
 		_target_progress = MAX_PROGRESS
-	if get_parent().progress_ratio > _target_progress:
-		get_parent().progress_ratio += delta * (speed * -1.0)
+	if EnemyPathA.progress_ratio > _target_progress:
+		EnemyPathA.progress_ratio += delta * (speed * -1.0)
 		ShipSprite.flip_h = true
 		_target_progress = MIN_PROGRESS
 
 # Hit
 func _take_damage(testName: StringName, amount: int, bulletFlag: bool) -> void:
+	print(testName)
 	if name == testName:
 		Health -= amount
 		ShipAnimationPlayer.play("Flash")
@@ -54,5 +54,4 @@ func _take_damage(testName: StringName, amount: int, bulletFlag: bool) -> void:
 		explosionEffect.global_position = global_position
 		explosionEffect.emitting = true
 		await get_tree().create_timer(1.0).timeout
-		# this needs to be better
-		get_parent().get_parent().queue_free()
+		queue_free()
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -9,8 +9,8 @@ const _SpawnQueue: Array[Dictionary] = [
 	#{ "time": 170.0, "type": 1, "location": Vector2(1180, 100), "rotation": 0.0 },
 	#{ "time": 170.0, "type": 1, "location": Vector2(100, 620), "rotation": 0.0 },
 	#{ "time": 170.0, "type": 1, "location": Vector2(1180, 620), "rotation": 0.0 },
-	{ "time": 180.0, "type": 2, "location": Vector2(0, 0), "rotation": 0.0 },
-	{ "time": 180.0, "type": 2, "location": Vector2(1280, 720), "rotation": 180.0 }
+	{ "time": 180.0, "type": 2, "location": Vector2(0, 0), "rotation": 0.0 }
+	#{ "time": 180.0, "type": 2, "location": Vector2(1280, 720), "rotation": 180.0 }
 ]
 
 # Get all enemies for the current time

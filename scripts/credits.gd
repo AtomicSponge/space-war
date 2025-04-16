3e0b07eba595e8d3451e1ff730c0a8beb9371d63
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-16 10:42:05 -0400

    sync

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,5 +1,23 @@
 extends Node
 
+const scroll_time: float = 20.0
+const scroll_stop: int = 1511
+
+@onready var CreditsScrollContainer: ScrollContainer = $CreditsScrollContainer
+@onready var ScrollTween: Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ScrollTween = create_tween()
+	ScrollTween.tween_property(CreditsScrollContainer, "scroll_vertical", scroll_stop, scroll_time)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if not ScrollTween.is_running():
+		CreditsScrollContainer.scroll_vertical = 0
+		ScrollTween = create_tween()
+		ScrollTween.tween_property(CreditsScrollContainer, "scroll_vertical", scroll_stop, scroll_time)
+
 # Check if a button or key is pressed and return to main menu
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey or InputEventJoypadButton or InputEventMouseButton:

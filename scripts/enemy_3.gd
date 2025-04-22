480649974a8dd8d7b3b9b2317f78a376186c1c13
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-22 10:07:45 -0400

    sync

--- scripts/enemy_3.gd
+++ scripts/enemy_3.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+@export var Explosion: PackedScene
+@export var speed: int = 350
+@export var ScoreValue: int = 100
+
 @onready var BladeSprite: Sprite2D = $BladeSprite
 @onready var BladeAnimationPlayer: AnimationPlayer = $BladeSprite/BladeAnimationPlayer
 @onready var EnemyHitbox: CollisionShape2D = $EnemyHitbox
@@ -8,9 +12,29 @@ var _is_ready: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	EnemyHitbox.set_deferred("disabled", true)
+	BladeAnimationPlayer.play("Fade")
+	await BladeAnimationPlayer.animation_finished
+	EnemyHitbox.set_deferred("disabled", false)
+	Events.enemy_hit.connect(_take_damage)
+	_is_ready = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not _is_ready:
 		return
+
+# Hit - update with health
+func _take_damage(testName: StringName, _amount: int, bulletFlag: bool) -> void:
+	if name == testName:
+		_is_ready = false
+		EnemyHitbox.set_deferred("disabled", true)
+		if bulletFlag == true:
+			GameState.PlayerScore += ScoreValue
+		BladeSprite.hide()
+		var explosionEffect = Explosion.instantiate()
+		add_child(explosionEffect)
+		explosionEffect.global_position = position
+		explosionEffect.emitting = true
+		await get_tree().create_timer(1.0).timeout
+		queue_free()

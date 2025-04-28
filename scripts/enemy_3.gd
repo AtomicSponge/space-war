c5c5c9c7657358871be99cc52969e8db162727bd
Matthew Evans <mevans+git@wtfsystems.net>
2025-04-28 10:50:56 -0400

    sync

--- scripts/enemy_3.gd
+++ scripts/enemy_3.gd
@@ -5,13 +5,17 @@ extends Path2D
 @export var Health: int = 100
 @export var ScoreValue: int = 100
 
+@onready var EnemyPath: PathFollow2D = $EnemyPath
 @onready var Saw: Area2D = $EnemyPath/Saw
 @onready var BladeSprite: Sprite2D = $EnemyPath/Saw/BladeSprite
 @onready var BladeAnimationPlayer: AnimationPlayer = $EnemyPath/Saw/BladeSprite/BladeAnimationPlayer
 @onready var EnemyHitbox: CollisionShape2D = $EnemyPath/Saw/EnemyHitbox
 @onready var rotateTween: Tween
+@onready var movementTween: Tween
 
 var _rotating: bool = false
+var _moving: bool = false
+var _foward_direction: bool = true
 var _is_ready: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -29,12 +33,24 @@ func _process(_delta: float) -> void:
 	if not _is_ready:
 		return
 	if not _rotating:
-		rotateTween = create_tween()
+		rotateTween = create_tween().set_trans(Tween.TRANS_SINE)
 		rotateTween.tween_property(BladeSprite, "rotation_degrees", 360.0, 0.1)
 		_rotating = true
 	if not rotateTween.is_running():
 		BladeSprite.rotation_degrees = 0
 		_rotating = false
+	if _foward_direction and not _moving:
+		movementTween = create_tween().set_trans(Tween.TRANS_SINE)
+		movementTween.tween_property(EnemyPath, "progress_ratio", 1.0, speed)
+		_foward_direction = false
+		_moving = true
+	if not _foward_direction and not _moving:
+		movementTween = create_tween().set_trans(Tween.TRANS_SINE)
+		movementTween.tween_property(EnemyPath, "progress_ratio", 0.0, speed)
+		_foward_direction = true
+		_moving = true
+	if not movementTween.is_running():
+		_moving = false
 
 # Hit
 func _take_damage(testName: StringName, amount: int, bulletFlag: bool) -> void:

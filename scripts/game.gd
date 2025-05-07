3828172770191bdba5e0e72575347e41fa392a53
Matthew Evans <mevans+git@wtfsystems.net>
2025-05-07 11:05:15 -0400

    sync

--- scripts/game.gd
+++ scripts/game.gd
@@ -21,6 +21,7 @@ var LastTick: float = 0.0
 
 # Called when a new game starts
 func NewGame() -> void:
+	SceneManager.BGMuisc[0].stop()
 	get_tree().paused = true
 	Player.set_position(StartPosition.position)
 	Player.hide()
@@ -35,9 +36,11 @@ func NewGame() -> void:
 	get_tree().paused = false
 	SpawnTimer.start()
 	GameState.GameStarted = true
+	SceneManager.BGMuisc[1].play()
 
 # Called at the end of a game
 func GameOver() -> void:
+	SceneManager.BGMuisc[1].stop()
 	GameState.GameStarted = false
 	get_tree().paused = true
 	Player.hide()
@@ -58,6 +61,7 @@ func GameOver() -> void:
 			do_high_score = true
 			break
 	GameState.PlayerScore = 0
+	SceneManager.BGMuisc[0].play()
 	if do_high_score:
 		SceneManager.SwitchScene("HighScores")
 	else:
--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -3,15 +3,13 @@ class_name NSceneManager
 
 # A collection of scenes in the game. Scenes are added through the Inspector panel
 @export var Scenes: Dictionary = {}
+@export var BGMuisc: Array[AudioStreamPlayer]
 
-@onready var BGMuisc = $BGMusic
 @onready var AniPlayer = $Fader/AnimationPlayer
 
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-func _process(_delta: float) -> void:
-	pass
+	SceneManager.BGMuisc[0].play()
 
 # Switch to the requested scene based on its alias
 # Parameter sceneAlias: The scene alias of the scene to switch to

0c4bdce9b108112875b37851c92de1122f9319b7
Matthew Evans <mevans+git@wtfsystems.net>
2025-05-09 15:00:20 -0400

    sync

--- scripts/options.gd
+++ scripts/options.gd
@@ -17,6 +17,9 @@ extends Node
 var CurrentDisplayMode: int = 0
 var CurrentLives: int = 0
 var CurrentContinues: int = 0
+var CurrentAudioVolume: float = 0.0
+var CurrentMusicVolume: float = 0.0
+var CurrentEffectsVolume: float = 0.0
 
 signal save_complete
 
@@ -25,6 +28,9 @@ func do_options_save() -> void:
 	GameState.MainVolume = db_to_linear(AudioServer.get_bus_volume_db(MasterBus))
 	GameState.MusicVolume = db_to_linear(AudioServer.get_bus_volume_db(MusicBus))
 	GameState.EffectsVolume = db_to_linear(AudioServer.get_bus_volume_db(EffectsBus))
+	CurrentAudioVolume = AudioScrollBar.value
+	CurrentMusicVolume = MusicScrollBar.value
+	CurrentEffectsVolume = EffectsScrollBar.value
 	GameState.NumberLives = int(LivesScrollBar.value)
 	GameState.NumberContinues = int(ContinueScrollBar.value)
 	CurrentLives = GameState.NumberLives
@@ -53,6 +59,9 @@ func _ready() -> void:
 	AudioScrollBar.value = db_to_linear(AudioServer.get_bus_volume_db(MasterBus))
 	MusicScrollBar.value = db_to_linear(AudioServer.get_bus_volume_db(MusicBus))
 	EffectsScrollBar.value = db_to_linear(AudioServer.get_bus_volume_db(EffectsBus))
+	CurrentAudioVolume = AudioScrollBar.value
+	CurrentMusicVolume = MusicScrollBar.value
+	CurrentEffectsVolume = EffectsScrollBar.value
 	LivesScrollBar.max_value = GameState.MAX_LIVES
 	LivesScrollBar.min_value = GameState.MIN_LIVES
 	LivesScrollBar.value = GameState.NumberLives
@@ -86,6 +95,9 @@ func _on_reset_button_pressed() -> void:
 		LivesScrollBar.value = GameState.DEFAULT_LIVES
 		GameState.NumberContinues = GameState.DEFAULT_CONTINUES
 		ContinueScrollBar.value = GameState.DEFAULT_CONTINUES
+		AudioScrollBar.value = CurrentAudioVolume
+		MusicScrollBar.value = CurrentMusicVolume
+		EffectsScrollBar.value = CurrentEffectsVolume
 		do_options_save()
 		dialog.queue_free()
 	)
@@ -109,6 +121,9 @@ func _on_back_button_pressed() -> void:
 			dialog.queue_free()
 		)
 		dialog.canceled.connect (func():
+			AudioServer.set_bus_volume_db(MasterBus, linear_to_db(CurrentAudioVolume))
+			AudioServer.set_bus_volume_db(MusicBus, linear_to_db(CurrentMusicVolume))
+			AudioServer.set_bus_volume_db(EffectsBus, linear_to_db(CurrentEffectsVolume))
 			save_complete.emit()
 			dialog.queue_free()
 		)
@@ -116,6 +131,12 @@ func _on_back_button_pressed() -> void:
 		dialog.popup_centered()
 		dialog.show()
 	if LivesScrollBar.value != CurrentLives or ContinueScrollBar.value != CurrentContinues:
+		changedDialog.call()
+	elif AudioScrollBar.value != CurrentAudioVolume:
+		changedDialog.call()
+	elif MusicScrollBar.value != CurrentMusicVolume:
+		changedDialog.call()
+	elif EffectsScrollBar.value != CurrentEffectsVolume:
 		changedDialog.call()
 	elif DisplayList.is_selected(0) and CurrentDisplayMode == 1:
 		changedDialog.call()
